c86f629f1d78cc2e6040e884efa1d9f5b3b85c34
Ngoc Nguyen <dev.fresher@greenglobal.vn>
2019-07-08 08:54:51 +0700

    add animation for introducing

--- src/App.js
+++ src/App.js
@@ -1,81 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Moment from "moment";
 import "./App.css";
 
-const App = () => {
-  const thatDay = "06/05/2019";
+class App extends Component {
+  constructor() {
+    super();
 
-  const [day, setDay] = useState("0");
-  const [hour, setHour] = useState("0");
-  const [minute, setMinute] = useState("0");
-  const [second, setSecond] = useState("0");
+    this.state = {
+      thatDay: "06/05/2019",
+      day: 0,
+      hour: 0,
+      minute: 0,
+      second: 0
+    }
+  }
 
-  useEffect(() => {
-    setInterval(() => {
-      let currentDate = new Date();
-      let thatDate = new Date(thatDay);
-      let intervals = (currentDate - thatDate) / 1000;
+  componentWillMount() {
+    this.timeInterval = setInterval(() => this.setTime(), 1000);
+  }
 
-      let day = Math.floor(intervals / (60 * 60 * 24));
-      day = day < 10 ? "0" + day : day;
-      intervals -= day * 60 * 60 * 24;
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
+  }
 
-      let hour = Math.floor(intervals / (60 * 60));
-      hour = hour < 10 ? "0" + hour : hour;
-      intervals -= hour * 60 * 60;
+  setTime = () => {
+    let {thatDay} = this.state;
 
-      let minute = Math.floor(intervals / 60);
-      minute = minute < 10 ? "0" + minute : minute;
-      intervals -= minute * 60;
+    let currentDate = new Date();
+    let thatDate = new Date(thatDay);
+    let intervals = (currentDate - thatDate) / 1000;
 
-      let second = Math.floor(intervals);
-      second = second < 10 ? "0" + second : second;
+    let day = Math.floor(intervals / (60 * 60 * 24));
+    day = day < 10 ? "0" + day : day;
+    intervals -= day * 60 * 60 * 24;
 
-      setDay(day);
-      setHour(hour);
-      setMinute(minute);
-      setSecond(second);
-    }, 1000);
-  });
+    let hour = Math.floor(intervals / (60 * 60));
+    hour = hour < 10 ? "0" + hour : hour;
+    intervals -= hour * 60 * 60;
 
-  return (
-    <div className="App">
+    let minute = Math.floor(intervals / 60);
+    minute = minute < 10 ? "0" + minute : minute;
+    intervals -= minute * 60;
+
+    let second = Math.floor(intervals);
+    second = second < 10 ? "0" + second : second;
+
+    this.setState({
+      day,
+      hour,
+      minute,
+      second
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
       <div className="group">
         <div className="introducing">
           <p>
-            Tính từ ngày {Moment(thatDay).format("DD-MM-YYYY")} thì đã được:
+            Tính từ ngày {Moment(this.state.thatDay).format("DD-MM-YYYY")} thì đã được:
           </p>
         </div>
         <div className="clock">
           <div className="clock-element day">
-            <p className="number">{day}</p>
+            <p className="number">{this.state.day}</p>
             <p className="text">NGÀY</p>
           </div>
           <div className="clock-element dot dot1">
             <span>:</span>
           </div>
           <div className="clock-element hour">
-            <p className="number">{hour}</p>
+            <p className="number">{this.state.hour}</p>
             <p className="text">GIỜ</p>
           </div>
           <div className="clock-element dot dot2">
             <span>:</span>
           </div>
           <div className="clock-element minute">
-            <p className="number">{minute}</p>
+            <p className="number">{this.state.minute}</p>
             <p className="text">PHÚT</p>
           </div>
           <div className="clock-element dot dot3">
             <span>:</span>
           </div>
           <div className="clock-element second">
-            <p className="number">{second}</p>
+            <p className="number">{this.state.second}</p>
             <p className="text">GIÂY</p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+    )
+  }
+}
 
 export default App;
